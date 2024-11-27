4080fb1ba1f815593425da833ec81a8fba06d8c3
20210298 <144835269+20210298@users.noreply.github.com>
2024-11-27 16:36:27 +0900

    🍱 선호도 출력

--- src/Container.js
+++ src/Container.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { FaStar } from "react-icons/fa"
 
 const Container=(props) => {
  const [lyricsExpanded, setLyricsExpanded] = useState(flase)
@@ -19,8 +20,14 @@ const Container=(props) => {
         rel="noreferrer">
           <div className="song-title">
             {`${props.song.title} (${props.song.singer})`}
-          </div>          
+          </div>  
         </a>
+        <div className="song-rating">
+          {[...Array(props.song.rating)].map((_, index) => (
+            <FaStar key={index}/>
+          ))
+          }
+        </div>        
       </div>
       {props.song.lyrics && lyricsExpanded && (
         <pre className="song-lyrics" 
